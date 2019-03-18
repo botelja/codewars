ee393f5bf73a33908dd8a9d5d4d354c53170ce94
botelja <ivo.surjak@gmail.com>
2019-03-18 05:57:00 +0100

    Solved 18-03-2019

---
+++ 18-03-19/string-ends-with.js
@@ -0,0 +1,31 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+function solution(str, ending) {
+    
+    const solution = str.slice(-ending.length);
+    
+    if(solution === ending) {
+        return true;
+    }
+    
+    return false;    
+}
+
+function solution(str, ending) {
+    
+    const solution = str.slice(-ending.length);
+    
+    return solution === ending ? true : false;    
+}
+
+function solution(str, ending) {
+    return str.endsWith(ending); 
+}
+
+console.log(solution('samurai', 'ai'), true)
+console.log(solution('abcde', 'abc'), false)
