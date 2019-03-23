e05031adf008cb558ce31d76c07ee4c9f7166e45
botelja <ivo.surjak@gmail.com>
2019-03-23 06:57:51 +0100

    Solved 23-03-19

---
+++ 23-03-19/changing-letters.js
@@ -0,0 +1,46 @@
+// When provided with a String, capitalize all vowels
+
+// For example:
+
+// Input : "Hello World!"
+
+// Output : "HEllO WOrld!"
+
+function swap(st) {
+    const stToArray = st.split('');
+
+    for(let i = 0; i < stToArray.length; i++) {
+       
+        switch(stToArray[i]) {
+            case 'a':
+                stToArray[i] = stToArray[i].toUpperCase();
+                break;
+            case 'e':
+               stToArray[i] = stToArray[i].toUpperCase();
+                break;
+            case 'i':
+               stToArray[i] = stToArray[i].toUpperCase();
+                break;
+            case 'o':
+               stToArray[i] = stToArray[i].toUpperCase();
+                break;
+            case 'u':
+               stToArray[i] = stToArray[i].toUpperCase();
+                break;
+        
+        }
+    }
+
+    return stToArray.join('');
+}
+
+function swap(st) {
+
+    const capitalizeVowels = st.split('').map((char) => 
+      
+        (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') ? char.toUpperCase() : char);
+
+    return capitalizeVowels.join('');
+}
+
+console.log(swap("HelloWorld!"),"HEllOWOrld!")
