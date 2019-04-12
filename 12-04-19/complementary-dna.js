e633a595a590dfce51ae489af10c5834cca14a6c
botelja <ivo.surjak@gmail.com>
2019-04-12 07:29:10 +0200

    Solved 12-04-19

--- 12-04-19/complementary-dna.js
+++ 12-04-19/complementary-dna.js
@@ -13,9 +13,22 @@
 function DNAStrand(dna){
     let dnaArr = dna.split('');
 
-    console.log(dna);
+    for(let i = 0; i < dnaArr.length; i++) {
+        if(dnaArr[i] === 'A') {
+            dnaArr[i] = 'T';
+        } else if(dnaArr[i] === 'T') {
+            dnaArr[i] = 'A';
+        }
+        if(dnaArr[i] === 'C') {
+            dnaArr[i] = 'G';
+        } else if(dnaArr[i] === 'G') {
+            dnaArr[i] = 'C';
+        }
+    }
+
+    return dnaArr.join('');
 }
 
 console.log(DNAStrand("AAAA"),"TTTT","String AAAA is");
-console.log(DNAStrand("ATTGC"),"TAACG","String ATTGC is");
-console.log(DNAStrand("GTAT"),"CATA","String GTAT is");
+//console.log(DNAStrand("ATTGC"),"TAACG","String ATTGC is");
+//console.log(DNAStrand("GTAT"),"CATA","String GTAT is");
