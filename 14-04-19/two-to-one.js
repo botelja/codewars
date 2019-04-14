29b759792bc48f43bb4169e6c67ed9866362b0f5
botelja <ivo.surjak@gmail.com>
2019-04-14 14:00:28 +0200

    14-04-19

---
+++ 14-04-19/two-to-one.js
@@ -0,0 +1,22 @@
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+
+// each taken only once - coming from s1 or s2.
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+    const longest = [];
+
+    const first = s1.split('');
+    const second = s2.split('');
+
+    console.log(first);
+
+}
+
+console.log(longest("aretheyhere", "yestheyarehere"), "aehrsty")
